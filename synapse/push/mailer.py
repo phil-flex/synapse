d748ec04e9ec74c6ad3a3c63aaeec3e3a27df144
phil <phil@flexsystem.com>
2018-09-11 21:09:21 +0800

    * reduce emailpusher check time ! fix email room invitation link

--- synapse/push/mailer.py
+++ synapse/push/mailer.py
@@ -575,6 +575,8 @@ class Mailer(object):
 
     def make_room_link(self, room_id):
         if self.hs.config.email_riot_base_url:
+            #base_url = self.hs.config.email_riot_base_url
+			#XXX: temporary fix for the broken Join room invitation link
             base_url = "%s/#/room" % (self.hs.config.email_riot_base_url)
         elif self.app_name == "Vector":
             # need /beta for Universal Links to work on iOS
